f0ebad53864b3af1767152f9fa33a4c3c48e8a55
AkhmadeevAA <sithalbert@ya.ru>
2020-05-09 23:04:27 +0300

    добавлен FileService

---
+++ references-analyzer/src/main/java/education/bert/rc/analyzer/file/service/FileService.java
@@ -0,0 +1,35 @@
+package education.bert.rc.analyzer.file.service;
+
+import education.bert.rc.analyzer.file.exception.FileAccessException;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileService {
+
+    private final Path uploadPath;
+
+    public FileService(String uploadPath) {
+        this.uploadPath = Paths.get(uploadPath);
+        try {
+            Files.createDirectories(this.uploadPath);
+        } catch (IOException e) {
+            throw new FileAccessException(e);
+        }
+    }
+
+    public List<String> readFile(String fileName) {
+        final Path file = uploadPath.resolve(fileName);
+        try (Stream<String> lines = Files.lines(file, StandardCharsets.UTF_8)) {
+            return lines.collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new FileAccessException(e);
+        }
+    }
+}
