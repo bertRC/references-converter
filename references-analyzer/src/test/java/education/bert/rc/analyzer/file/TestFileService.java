11da3b7963d6a7cffba2f02e3955453bbbcb0419
AkhmadeevAA <sithalbert@ya.ru>
2020-05-09 23:40:43 +0300

    добавлен тест на остальные методы класса FileService

--- references-analyzer/src/test/java/education/bert/rc/analyzer/file/TestFileService.java
+++ references-analyzer/src/test/java/education/bert/rc/analyzer/file/TestFileService.java
@@ -1,5 +1,6 @@
 package education.bert.rc.analyzer.file;
 
+import education.bert.rc.analyzer.file.exception.FileAccessException;
 import education.bert.rc.analyzer.file.service.FileService;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestFileService {
 
+    private final FileService fileService = new FileService("testUpload");
+
     @Test
     public void testReadFile() {
-        final FileService fileService = new FileService("testUpload");
         final List<String> expectedList = new ArrayList<>();
         expectedList.add("Line 1");
         expectedList.add("Line 2");
@@ -21,5 +24,15 @@ public class TestFileService {
         final List<String> actualList = fileService.readFile("testFile.txt");
 
         assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    public void testWrongUploadPath() {
+        assertThrows(FileAccessException.class, () -> new FileService("X:/test"));
+    }
+
+    @Test
+    public void testWrongFile() {
+        assertThrows(FileAccessException.class, () -> fileService.readFile(""));
     }
 }
