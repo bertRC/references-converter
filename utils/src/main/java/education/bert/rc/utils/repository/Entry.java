d30c3f7942281aab6dacef0be0232079768113ed
AkhmadeevAA <sithalbert@ya.ru>
2020-05-16 15:22:00 +0300

    начато внедрение плагина lombok

--- utils/src/main/java/education/bert/rc/utils/repository/Entry.java
+++ utils/src/main/java/education/bert/rc/utils/repository/Entry.java
@@ -1,43 +1,9 @@
 package education.bert.rc.utils.repository;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Entry {
     private final String key;
     private final String value;
-
-    public Entry(String key, String value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    @Override
-    public String toString() {
-        return "Entry{" +
-                "key='" + key + '\'' +
-                ", value='" + value + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Entry entry = (Entry) o;
-        return Objects.equals(key, entry.key) &&
-                Objects.equals(value, entry.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(key, value);
-    }
 }
