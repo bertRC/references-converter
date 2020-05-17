082827b7351ccba0407f08d3f74b16395c0bb859
AkhmadeevAA <sithalbert@ya.ru>
2020-05-17 22:22:14 +0300

    теперь подставим аннотацию NonNull

--- references-generator/src/main/java/education/bert/rc/generator/templates/MyClass.java
+++ references-generator/src/main/java/education/bert/rc/generator/templates/MyClass.java
@@ -1,12 +1,13 @@
 package education.bert.rc.generator.templates;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class MyClass {
 
-    private final String prefix;
-    private final String suffix;
+    @NonNull private final String prefix;
+    @NonNull private final String suffix;
 
     public String concat() {
         return prefix + suffix;
