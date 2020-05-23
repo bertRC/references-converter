a29424d079b17f754a1f6aab3d35ff33a0cf7cc6
AkhmadeevAA <sithalbert@ya.ru>
2020-05-24 00:00:54 +0300

    пробуем utf-8

--- webapp/src/main/java/education/bert/rc/webapp/servlet/HelloServlet.java
+++ webapp/src/main/java/education/bert/rc/webapp/servlet/HelloServlet.java
@@ -11,8 +11,8 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        req.setCharacterEncoding("UTF-8");
-        resp.setCharacterEncoding("UTF-8");
+//        req.setCharacterEncoding("UTF-8");
+//        resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/plain;charset=UTF-8");
 
         String message = "Привет мир!";
