9585687543b4d1f334215a46fba4ad15ff854dfd
AkhmadeevAA <sithalbert@ya.ru>
2020-05-23 22:35:22 +0300

    небольшой фикс

--- webapp/src/main/java/education/bert/rc/webapp/servlet/HelloServlet.java
+++ webapp/src/main/java/education/bert/rc/webapp/servlet/HelloServlet.java
@@ -7,11 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(
-        name = "MyServlet",
-        urlPatterns = {"/hello"}
-)
+@WebServlet(name = "MyServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         ServletOutputStream out = resp.getOutputStream();
