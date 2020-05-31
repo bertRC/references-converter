2734deb854a4805138b0e0e023c25473c9b1fa98
AkhmadeevAA <sithalbert@ya.ru>
2020-05-31 17:57:40 +0300

    сервлет главной страницы с минимальным функционалом реализован

--- webapp/src/main/java/education/bert/rc/webapp/servlet/IndexServlet.java
+++ webapp/src/main/java/education/bert/rc/webapp/servlet/IndexServlet.java
@@ -1,5 +1,10 @@
 package education.bert.rc.webapp.servlet;
 
+import education.bert.rc.analyzer.service.Analyzer;
+import education.bert.rc.generator.collection.TemplateCollection;
+import education.bert.rc.utils.misc.Separator;
+import education.bert.rc.utils.repository.Bibliography;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,11 +12,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 @WebServlet(name = "IndexServlet", urlPatterns = "")
 public class IndexServlet extends HttpServlet {
+
+    private Analyzer analyzer;
+    private TemplateCollection templateCollection;
+
+    @Override
+    public void init() {
+        analyzer = (Analyzer) this.getServletContext().getAttribute("analyzer");
+        templateCollection = (TemplateCollection) this.getServletContext()
+                .getAttribute("templateCollection");
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,11 +35,16 @@ public class IndexServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         HttpSession session = req.getSession();
-        final String text = req.getParameter("inputText");
-//        System.out.println(text);
-        final List<String> strings = Arrays.asList(text.split("(\r\n)|(\n\r)|[\r\n]"));
-//        this.getServletContext().setAttribute("strings", strings);
-        session.setAttribute("strings", strings);
-        resp.sendRedirect("/resultDemo");
+        final String inputText = req.getParameter("inputText");
+        if (!inputText.isEmpty()) {
+            final List<String> inputStrings = Separator.separate(inputText);
+            final List<Bibliography> bibliographies = analyzer.analyze(inputStrings);
+            final List<String> results = templateCollection.getAll().get(0).generate(bibliographies);
+
+            session.setAttribute("lineSeparator", Separator.getLineSeparator(inputText));
+            session.setAttribute("bibliographies", bibliographies);
+            session.setAttribute("results", results);
+        }
+        resp.sendRedirect("/result");
     }
 }
