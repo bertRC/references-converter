72eeee4039714590cd62a83dea725cb3fe7a5a1b
AkhmadeevAA <sithalbert@ya.ru>
2020-06-01 21:40:51 +0300

    частично реализован сервлет страницы результатов

--- webapp/src/main/java/education/bert/rc/webapp/servlet/ResultServlet.java
+++ webapp/src/main/java/education/bert/rc/webapp/servlet/ResultServlet.java
@@ -5,13 +5,26 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ResultServlet", urlPatterns = "/result")
+@WebServlet(name = "ResultServlet", urlPatterns = {"/result", "/copyResult"})
 public class ResultServlet extends HttpServlet {
+
+    private String resultsInLine;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("result.jsp").forward(req, resp);
+        switch (req.getRequestURI()) {
+            case "/result":
+                HttpSession session = req.getSession();
+                resultsInLine = (String) session.getAttribute("resultsInLine");
+                req.getRequestDispatcher("result.jsp").forward(req, resp);
+                break;
+            case "/copyResult":
+                if (resultsInLine != null) {
+                    resp.getWriter().write(resultsInLine);
+                }
+        }
     }
 }
